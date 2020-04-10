3479fafa568ede3ada26d8a7dedbaaf0c6d393a5
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-10 15:55:32 +0900

    added shell for channel list

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-    <img src="https://uikit.lewagon.com/assets/logo-0c157df32d93155001ae8d8b1b7740b3082e698b4ad0cc91792e8725deb68d85.png" alt="" id="logo"/>
+      <img src="https://uikit.lewagon.com/assets/logo-0c157df32d93155001ae8d8b1b7740b3082e698b4ad0cc91792e8725deb68d85.png" alt="" id="logo"/>
+      <div className="ChannelList"></div>
+      <div className="MessageList"></div>
     </div>
   );
 }
