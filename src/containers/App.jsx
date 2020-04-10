0b4702dec6e1cccad7a99e7ef580dbdf3fa169b7
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-10 20:16:38 +0900

    added static component headers

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <img src="https://uikit.lewagon.com/assets/logo-0c157df32d93155001ae8d8b1b7740b3082e698b4ad0cc91792e8725deb68d85.png" alt="" id="logo"/>
-      <div className="ChannelList"></div>
+      <div className="LogoHolder">
+        <img src="https://uikit.lewagon.com/assets/logo-0c157df32d93155001ae8d8b1b7740b3082e698b4ad0cc91792e8725deb68d85.png" alt="" id="logo"/>
+      </div>
+      <div className="ChannelList">Redux Chat</div>
       <div className="MessageList">
-        <div className="ChannelTitle"></div>
+        <div className="ChannelTitle">Channel</div>
         <div className="Messages"></div>
         <div className="MessageForm">
           <form class="form-inline">
-            <div class="form-group mx-sm-3 mb-2">
+            <div class="form-group">
               <input type="message" class="form-control" id="inputMessage"></input>
-              <button type="submit" class="btn btn-danger">Send</button>
+              <button type="submit" class="btn btn-danger" id="messageButton">Send</button>
             </div>
           </form>
         </div>
