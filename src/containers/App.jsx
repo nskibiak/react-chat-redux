b1fe942853b83c2732732e37a0356c5bc270e2c1
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-10 16:09:36 +0900

    finished messages shells

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,7 +5,11 @@ function App() {
     <div className="App">
       <img src="https://uikit.lewagon.com/assets/logo-0c157df32d93155001ae8d8b1b7740b3082e698b4ad0cc91792e8725deb68d85.png" alt="" id="logo"/>
       <div className="ChannelList"></div>
-      <div className="MessageList"></div>
+      <div className="MessageList">
+        <div className="ChannelTitle"></div>
+        <div className="Messages"></div>
+        <div className="MessageForm"></div>
+      </div>
     </div>
   );
 }
