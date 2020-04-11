a9e543e7ae8d718fa08108ec041e8ac069279cd2
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-11 15:13:04 +0900

    added image to public folder and corrected link in app.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="LogoHolder">
-        <img src="https://uikit.lewagon.com/assets/logo-0c157df32d93155001ae8d8b1b7740b3082e698b4ad0cc91792e8725deb68d85.png" alt="" id="logo"/>
+        <img src="/lewagon-logo.png" alt="" id="logo"/>
       </div>
       <div className="ChannelList">Redux Chat</div>
       <MessageList />
