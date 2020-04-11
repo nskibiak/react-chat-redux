f36bff7b1de8f4cf83ed1913fa5205e3fb2d5297
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-11 14:35:15 +0900

    finished styling messages

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class App extends Component {
 
   render() {
-    return 'hello';
+    return (
+      <div className="Message">
+        <div className="topLine">
+          <div className="userName">{this.props.message.author}&nbsp;&nbsp;</div><div className="timeStamp">- {this.props.message.created_at.slice(11,19)}</div>
+        </div>
+        <div className="messageContent">{this.props.message.content}</div>
+      </div>
+    );
   };
 };
 
--- src/reducers/current_user_reducer.js
+++ src/reducers/current_user_reducer.js
@@ -1,6 +1,6 @@
 const CurrentUserReducer = (state, action) => {
   if (state === undefined) {
-    return prompt("Please enter a username") || `anonymous${Math.floor(10 + (Math.random() * 90))}`;
+    return `anonymous${Math.floor(10 + (Math.random() * 90))}`;
   }
 
   if (action.type === 'LIST_USER') {
