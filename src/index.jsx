05f8b65dd645cec0c2cd4e1340817546dee326f3
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-11 15:05:05 +0900

    connected initialState to createStore

--- src/index.jsx
+++ src/index.jsx
@@ -22,19 +22,31 @@ const reducers = combineReducers({
   selectedChannel: SelectedChannelReducer
 });
 
+
+const initialState = {
+  messages: [
+    {
+      "author":"anonymous92",
+      "content":"Hello world!",
+      "created_at":"2017-09-26T23:03:16.365Z"
+    },
+    {
+      "author":"anonymous77",
+      "content":"My name is anonymous77",
+      "created_at":"2017-09-26T23:03:21.194Z"
+    }
+  ],
+  channels: [ 'general', 'react', 'paris' ],
+  currentUser: prompt("Please enter a username") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+};
+
 const middlewares = applyMiddleware(reduxPromise, logger);
 
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={createStore(reducers, initialState, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
 );
 
-
-// const initialState = {
-//   messages: [],
-//   channels: [ 'general', 'react', 'paris' ],
-//   currentUser: prompt("Please enter a username") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
-//   selectedChannel: 'general'
-// };
