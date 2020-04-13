da1710f11bd3d206a1ae177c46b12e48ac206a91
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-13 14:03:27 +0900

    made submit form a controlled component

--- src/containers/message_form.jsx
+++ src/containers/message_form.jsx
@@ -3,12 +3,29 @@ import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 
 class MessageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="MessageForm">
-        <form className="form-inline">
+        <form className="form-inline" onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <input type="message" className="form-control" id="inputMessage"></input>
+            <input type="message" className="form-control" id="inputMessage" value={this.state.value} onChange={this.handleChange}></input>
             <button type="submit" className="btn btn-danger" id="messageButton">Send</button>
           </div>
         </form>
