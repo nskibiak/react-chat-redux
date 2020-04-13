2ff13d9dce2240825ed473e957c1270b8e9e7b32
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-13 14:13:28 +0900

    completed POST request for CREATE MESSAGE action

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,22 @@ export function fetchMessages(channel) {
   };
 }
 
+export const CREATE_MESSAGE = 'CREATE_MESSAGE';
+
 export function createMessage(channel, author, content) {
-  // TODO
+  const body = { author: {author}, content: {content} };
+  const promise = fetch(`https://wagon-chat.herokuapp.com/${channel}/messages`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }).then(r => r.json());
+
+  return {
+    type: CREATE_MESSAGE,
+    payload: promise
+  };
 }
 
