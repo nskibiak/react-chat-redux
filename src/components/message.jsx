b0e8c755b899deb48cb99eaeff376405c3460fa7
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-15 10:13:59 +0900

    now with emojis

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -26,7 +26,9 @@ class App extends Component {
         <div className="topLine">
           <div className="userName" style={ styles }>{this.props.message.author}&nbsp;&nbsp;</div><div className="timeStamp">- {this.props.message.created_at.slice(11,19)}</div>
         </div>
-        <div className="messageContent">{this.props.message.content}</div>
+        <div className="messageContent">
+          <p>{emojify(this.props.message.content)}</p>
+        </div>
       </div>
     );
   };
